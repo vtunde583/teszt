426a9bb7b0c9b2eeae33744e792c5969bb6f2020
vtunde583 <vartun583@hengersor.hu>
2024-09-23 09:56:37 +0200

    teszt
    dolgozat laravel

---
+++ teszt/resources/views/bejelentkezes.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bejelentkezes</title>
+</head>
+<body>
+    <h2>Kérlek ide majd írd le a felhasználó neved és a jelszót. </h2>
+</body>
+</html>
---
+++ teszt/resources/views/felhasznalo.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Felhasznalo</title>
+</head>
+<body>
+    <h2>Ez az oldal a felhasználókat írja le</h2>
+</body>
+</html>
